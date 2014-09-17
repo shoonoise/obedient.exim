f5e3c3337c2abced2eb3c44eaacf99a8ce3a8d15
shoonoise <avkushnarev@gmail.com>
2014-09-17 13:40:38 +0400

    Bump version: 1.1 → 1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='obedient.exim',
-        version='1.1',
+        version='1.2',
         url='https://github.com/yandex-sysmon/obedient-exim',
         license='GPLv3',
         author='Nikolay Bryskin',
